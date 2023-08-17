1e0eab9419fd21690289f46e13313b80ebe8bfb4
Hanif Nur Amrullah <amrullahhanifnur@gmail.com>
2023-08-17 11:52:04 +0700

    add netlify deployment

--- server.ts
+++ server.ts
@@ -1,7 +1,12 @@
+import { createRequestHandler } from "@netlify/remix-adapter";
 import * as build from "@remix-run/dev/server-build";
 import { installGlobals } from "@remix-run/node";
-import { createRequestHandler } from "@remix-run/vercel";
+import sourceMapSupport from "source-map-support";
 
+sourceMapSupport.install();
 installGlobals();
 
-export default createRequestHandler({ build, mode: process.env.NODE_ENV });
+export const handler = createRequestHandler({
+  build,
+  mode: process.env.NODE_ENV,
+});
